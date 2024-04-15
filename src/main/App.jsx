da1eab20302b3441951cb09f301c5debb7a899c8
okarlheinz <okarlheinz@outlook.com>
2024-04-14 21:26:40 -0300

    Secao 24 Projeto Cadastro de Usuario aula 428

---
+++ src/main/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/templates/Logo'
+import Nav from '../components/templates/Nav'
+import Home from '../components/home/Home'
+import Footer from '../components/templates/Footer'
+
+export default props =>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Home />
+        <Footer />
+    </div>
